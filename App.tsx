341a049b9b02383f7606cf416588b6acc4279aaa
Leon Segal <leonsegal1@gmail.com>
2020-05-07 18:24:52 +0100

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,19 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Button, Text, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+    const [result, setResult] = useState('');
+    const [isStraight, setIsStraight] = useState(false);
+
+    // animation?
+    const handlePress = () => {
+        setResult(Math.floor(Math.random() * 2) ? 'left' : 'right');
+    }
+
+    return (
+        <View style={styles.container}>
+            <Button
+                onPress={handlePress}
+                title="Choose"
+                color={'hsl(120, 40%, 30%)'}
+            />
+            <Text style={{marginVertical: 50}}>{result ? `${result}!` : `Left ${isStraight ? ', straight' : ''} or right?`}</Text>
+            <Button
+                onPress={() => {
+                    setResult('');
+                    setIsStraight(!isStraight)
+                }}
+                title="toggle straight ahead"
+                color={'hsl(240, 40%, 30%)'}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'hsl(120, 5%, 95%)',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
