83ff8495f048c9ed9d25544644d664af7b61d018
Leon Segal <leonsegal1@gmail.com>
2020-05-08 10:59:23 +0100

    add direction icons

--- App.tsx
+++ App.tsx
@@ -1,30 +1,48 @@
-import React, {useState} from 'react';
-import {StyleSheet, Button, Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, Button, Text, View, Image} from 'react-native';
 
 export default function App() {
-    const [result, setResult] = useState('');
-    const [isStraight, setIsStraight] = useState(false);
+    const [direction, setDirection] = useState('');
+    const [title, setTitle] = useState('Left...or right?');
+    const timeout = 2000;
 
-    // animation?
+    useEffect(() => {
+        if (direction) {
+            const timer = setTimeout(() => {
+                setDirection('');
+                setTitle('Left...or right?')
+            }, timeout);
+
+            return () => clearTimeout(timer);
+        }
+    }, [direction]);
+
     const handlePress = () => {
-        setResult(Math.floor(Math.random() * 2) ? 'left' : 'right');
+        setTitle('');
+        setDirection(
+            Math.floor(Math.random() * 2)
+                ? 'left'
+                : 'right'
+        );
     }
 
     return (
         <View style={styles.container}>
+            <View style={{flexDirection: 'row', justifyContent: "space-between"}}>
+                <Text style={{marginVertical: 30, fontWeight: 400, fontSize: 20}}>{title}</Text>
+                {direction === 'left'
+                    ? <Image style={{width: 80, height: 40, marginVertical: 30}} source={require('./assets/left.png')}/>
+                    : null}
+
+                {direction === 'right'
+                    ?
+                    <Image style={{width: 80, height: 40, marginVertical: 30}} source={require('./assets/right.png')}/>
+                    : null}
+            </View>
             <Button
                 onPress={handlePress}
                 title="Choose"
                 color={'hsl(120, 40%, 30%)'}
-            />
-            <Text style={{marginVertical: 50}}>{result ? `${result}!` : `Left ${isStraight ? ', straight' : ''} or right?`}</Text>
-            <Button
-                onPress={() => {
-                    setResult('');
-                    setIsStraight(!isStraight)
-                }}
-                title="toggle straight ahead"
-                color={'hsl(240, 40%, 30%)'}
             />
         </View>
     );
